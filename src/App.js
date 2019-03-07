434a769780a5209edaf14b3a6810f07e6b1f907c
Mujeeb <mujib517@gmail.com>
2019-03-07 08:05:32 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Footer />
+            </div>);
+    }
 }
-
-export default App;
