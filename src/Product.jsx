d4eb8d0d1accf111728d8b76642a32c266aefad0
Mujeeb <mujib517@gmail.com>
2019-03-26 08:02:22 +0530

    Delete

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
-// function product(props) {
-//     return <div>
-//         <h4>{brand} {model}</h4>
-//         <div>${price}</div>
-//         <div>InStock? : {inStock ? "Yes" : "No"}</div>
-//         <hr />
-//     </div>
-// }
+export default class Product extends React.Component {
 
-// export default product;
+    onDeleteClick(id) {
+        axios.delete("https://exp-rest-api.herokuapp.com/api/products/" + id)
+            .then(() => console.log("deleted"))
+            .catch(() => console.log("error"));
+    }
 
-export default (props) => {
-
-    const { brand, model, image, price, inStock } = props.data;
-    // const product = props.data;
-    return <div>
-        <h4>{brand} {model}</h4>
-        {/* <img src={image} width="200" height="200" class="img img-thumbnail" /> */}
-        <div>${price}</div>
-        <div>InStock? : {inStock ? "Yes" : "No"}</div>
-        <hr />
-    </div>
+    render() {
+        const { _id, brand, model, price, inStock } = this.props.data;
+        // const product = props.data;
+        return <div>
+            <h4>{brand} {model}</h4>
+            {/* <img src={image} width="200" height="200" class="img img-thumbnail" /> */}
+            <div>${price}</div>
+            <div>InStock? : {inStock ? "Yes" : "No"}</div>
+            <button class="btn btn-sm btn-danger" onClick={() => this.onDeleteClick(_id)}>Delete</button>
+            <hr />
+        </div>
+    }
 }
