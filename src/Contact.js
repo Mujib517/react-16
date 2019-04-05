ccb699819522c8ac0ed26baa869784b2dce37f2b
Mujeeb <mujib517@gmail.com>
2019-04-05 07:40:07 +0530

    React material UI Lib

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,61 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 
 export default (props) => {
-    return <h1>Contact Page</h1>
+    return (<div>
+        <h1>Contact Page</h1>
+        <Grid container spacing={24}>
+            <Grid item xs={12}>
+                <Paper>xs=12</Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper>xs=6</Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper>xs=6</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper>
+                    <List component="nav">
+                        <ListItem button>
+                            <ListItemIcon>
+                                <InboxIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Inbox" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemIcon>
+                                <DraftsIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Drafts" />
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <List component="nav">
+                        <ListItem button>
+                            <ListItemText primary="Trash" />
+                        </ListItem>
+                    </List>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper>xs=3</Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper>xs=3</Paper>
+            </Grid>
+        </Grid>
+    </div>)
 };
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default class Home extends React.Component {
 
@@ -14,7 +22,30 @@ export default class Home extends React.Component {
     }
 
     render() {
-        return <h1>{this.state.count}</h1>
+        return (<div><h1>{this.state.count}</h1>
+
+            <Button>Click Me</Button>
+            <Button variant="contained" color="primary">Click Me</Button>
+            <Button variant="contained" color="secondary">Click Me</Button>
+            <Button variant="outlined" color="secondary">Click Me</Button>
+            <Fab variant="contained" color="secondary">
+                <AddIcon />
+            </Fab>
+
+            <Card>
+                <CardContent>
+                    <span>THis is a card</span>
+                </CardContent>
+                <CardActionArea>
+                    <IconButton aria-label="Delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </CardActionArea>
+            </Card>
+
+
+
+        </div>)
     }
 
     componentWillUnmount() {
